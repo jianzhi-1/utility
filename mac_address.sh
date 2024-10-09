03c83b35f468ff6d62b6050c29553f77ee894db5
Jianzhi Wang <jianzhiw00@gmail.com>
2024-10-08 20:47:01 -0700

    Create mac_address.sh

---
+++ mac_address.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ip link | grep -o "link/ether [0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]" > mac_address.txt
+cat mac_address.txt | grep -o "[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]"
